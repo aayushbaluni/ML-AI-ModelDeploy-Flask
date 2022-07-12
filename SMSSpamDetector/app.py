41e99c5681d1626ccbe425a28a6ec8218c3cd331
Ayush Baluni <73417844+aayushbaluni@users.noreply.github.com>
2022-07-12 13:25:16 +0530

    SMS SPAM DETECTOR

---
+++ SMSSpamDetector/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+import numpy as np
+import pickle
+
+app = Flask((__name__))
+model = pickle.load(open('modelll.pkl', 'rb'))
+cv=pickle.load(open('cv.pkl','rb'))
+dic={1:'SPAM',0:'NOT SPAM'}
+@app.route('/')
+def home():
+	return render_template('index.html')
+
+
+
+@app.route('/check',methods=['POST'])
+def check():
+	msg=request.form['message']
+	#msg.replace('[^a-zA-Z]',' ',regex=True,inplace=True)
+	msg=[msg]
+	msg=cv.transform(msg).toarray()
+	pre=model.predict(msg)
+	return render_template('index.html',prediction="According to Me: {}".format(dic[pre[0]]))
+
+if __name__ == '__main__':
+	app.run(debug=True)
+
+
+
